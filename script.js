bb7d6ffe8fb71d9ab42328010709fa697697144f
Cepha0Z <154107032+Cepha0Z@users.noreply.github.com>
2025-06-06 13:44:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'correct number';
+// document.querySelector('.number').textContent = 'gg';
+// document.querySelector('.guess').textContent = 'sdfhlsdf';
+// console.log(document.querySelector('.guess').textContent);
+
+let secert = Math.trunc(Math.random() * 20) + 1;
+let Score = 20;
+let HScore = 0;
+console.log(document.querySelector('.score').textContent);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (Score >= 1) {
+    if (!guess) {
+      document.querySelector('.message').textContent = 'Enter Valid Data';
+    }
+
+    if (guess === secert) {
+      document.querySelector('.number').textContent = secert;
+      document.querySelector('.message').textContent = 'Woohoo Correct Number';
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '40rem';
+      if (Score > HScore) HScore = Score;
+      document.querySelector('.highscore').textContent = HScore;
+    } else if (guess > secert) {
+      document.querySelector('.message').textContent = 'Too High';
+      Score--;
+      document.querySelector('.score').textContent = Score;
+    } else if (guess < secert) {
+      document.querySelector('.message').textContent = 'Too Low';
+      Score--;
+      document.querySelector('.score').textContent = Score;
+    }
+  } else {
+    document.querySelector('.message').textContent = 'You LOSEEE!!!!!';
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.guess').value = '';
+
+  secert = Math.trunc(Math.random() * 20) + 1;
+  Score = 20;
+  document.querySelector('.number').textContent = secert;
+  document.querySelector('.score').textContent = Score;
+});
